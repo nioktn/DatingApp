74d87e72f4be3d485f77a0988c842c054286c894
Nikita Nikitin <nioktn@gmail.com>
2019-08-19 16:50:05 +0300

    Initial Commit

---
+++ DatingApp.API/Persistance/ValuesContext.cs
@@ -0,0 +1,12 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Persistance
+{
+    public class ValuesContext : DbContext
+    {
+        public ValuesContext(DbContextOptions options) : base (options) { }
+                
+        public DbSet<Value> Values { get; set; }
+    }
+}
